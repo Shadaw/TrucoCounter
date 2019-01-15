968242b359b78f9ddb6fcfa5abf4865eb11da399
Shadaw <blackshadowm1379@gmail.com>
2019-01-15 16:58:50 -0200

    Updates

---
+++ precache-manifest.c206fd08a3baabe9183a4a34eda1a19e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8cabc123e35129cbd9f8",
+    "url": "/TrucoCounter/static/js/runtime~main.8cabc123.js"
+  },
+  {
+    "revision": "62f475a4cf3a953afb5b",
+    "url": "/TrucoCounter/static/js/main.62f475a4.chunk.js"
+  },
+  {
+    "revision": "2b57cec28314f93b5ac8",
+    "url": "/TrucoCounter/static/js/1.2b57cec2.chunk.js"
+  },
+  {
+    "revision": "a7fccfb2a26c79a7b5e16db5380b3d21",
+    "url": "/TrucoCounter/index.html"
+  }
+];
